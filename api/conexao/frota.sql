9f3fe594f085b60355df882c8052314764692428
Victorregis885 <victorregis5002@gmail.com>
2023-11-22 19:03:00 -0300

    alteracao BD

--- api/conexao/frota.sql
+++ api/conexao/frota.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22/10/2023 às 18:33
+-- Tempo de geração: 22/11/2023 às 23:01
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.0.28
 
@@ -36,17 +36,17 @@ CREATE TABLE `manutencao` (
   `descricao` varchar(255) NOT NULL,
   `veiculo_id_veiculo` int(11) NOT NULL,
   `oficina_id_oficina` int(11) NOT NULL,
-  `pecas_id_peca` int(11) NOT NULL
+  `pecas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Despejando dados para a tabela `manutencao`
 --
 
-INSERT INTO `manutencao` (`id_manutencao`, `data`, `valor_servico`, `valor_pecas`, `valor_total`, `descricao`, `veiculo_id_veiculo`, `oficina_id_oficina`, `pecas_id_peca`) VALUES
-(1, '2023-10-11 10:16:44', 10, 10, 20, 'troca de oleo ', 3, 1, 1),
-(7, '2023-10-11 10:17:00', 30, 10, 40, 'troca de pastilha de freio ', 5, 1, 1),
-(12, '2023-10-11 20:19:29', 12, 12, 24, 'trocar amortecedor', 3, 1, 1);
+INSERT INTO `manutencao` (`id_manutencao`, `data`, `valor_servico`, `valor_pecas`, `valor_total`, `descricao`, `veiculo_id_veiculo`, `oficina_id_oficina`, `pecas`) VALUES
+(1, '2023-11-22 19:00:45', 10, 10, 20, 'troca de oleo ', 3, 1, 3),
+(12, '2023-11-22 19:00:39', 12, 12, 24, 'trocar amortecedor', 3, 1, 2),
+(14, '2023-11-22 19:00:30', 150, 25.2, 175.2, 'trocar oleio', 5, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -55,7 +55,7 @@ INSERT INTO `manutencao` (`id_manutencao`, `data`, `valor_servico`, `valor_pecas
 --
 
 CREATE TABLE `oficina` (
-  `id_oficina` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nome_oficina` varchar(45) NOT NULL,
   `telefone` varchar(45) NOT NULL,
   `rua` varchar(45) NOT NULL,
@@ -67,7 +67,7 @@ CREATE TABLE `oficina` (
 -- Despejando dados para a tabela `oficina`
 --
 
-INSERT INTO `oficina` (`id_oficina`, `nome_oficina`, `telefone`, `rua`, `bairro`, `numero`) VALUES
+INSERT INTO `oficina` (`id`, `nome_oficina`, `telefone`, `rua`, `bairro`, `numero`) VALUES
 (1, 'oficina do clebin', '14-99999-9999', 'raimundo de morais', 'sÃ£o joao', 304),
 (3, 'oficina do dida', '14-99999-9999', '11 de setembro', 'gilmar torres 2', 515),
 (4, 'mercedes auto center', '14-99170-7070', 'machado de assis', 'palmeiras', 5568);
@@ -79,7 +79,7 @@ INSERT INTO `oficina` (`id_oficina`, `nome_oficina`, `telefone`, `rua`, `bairro`
 --
 
 CREATE TABLE `pecas` (
-  `id_peca` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nome_peca` varchar(45) NOT NULL,
   `fabricante_peca` varchar(45) NOT NULL,
   `preco` float NOT NULL
@@ -89,31 +89,10 @@ CREATE TABLE `pecas` (
 -- Despejando dados para a tabela `pecas`
 --
 
-INSERT INTO `pecas` (`id_peca`, `nome_peca`, `fabricante_peca`, `preco`) VALUES
-(1, 'pastilha de freio', 'ford', 5);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `revisao`
---
-
-CREATE TABLE `revisao` (
-  `id_revisao` int(11) NOT NULL,
-  `data_revisao` datetime NOT NULL,
-  `valor_revisao` float NOT NULL,
-  `descricao` varchar(255) NOT NULL,
-  `veiculo_id_veiculo` int(11) NOT NULL,
-  `oficina_id_oficina` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Despejando dados para a tabela `revisao`
---
-
-INSERT INTO `revisao` (`id_revisao`, `data_revisao`, `valor_revisao`, `descricao`, `veiculo_id_veiculo`, `oficina_id_oficina`) VALUES
-(2, '2023-06-29 16:13:10', 100, 'precisa de troca de Ã³leo', 3, 3),
-(3, '2023-06-29 16:13:59', 100, 'precisa de troca de Ã³leo e pastilha de freio', 3, 3);
+INSERT INTO `pecas` (`id`, `nome_peca`, `fabricante_peca`, `preco`) VALUES
+(1, 'pastilha de freio', 'ford', 5),
+(2, 'Amortecedor dianteiro ', 'Wiltec', 710.3),
+(3, 'Oleo 10w40', 'X1 maxx', 20.65);
 
 -- --------------------------------------------------------
 
@@ -122,7 +101,7 @@ INSERT INTO `revisao` (`id_revisao`, `data_revisao`, `valor_revisao`, `descricao
 --
 
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nome_usuario` varchar(45) NOT NULL,
   `login` varchar(45) NOT NULL,
   `senha` varchar(45) NOT NULL,
@@ -133,7 +112,7 @@ CREATE TABLE `usuario` (
 -- Despejando dados para a tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `login`, `senha`, `email`) VALUES
+INSERT INTO `usuario` (`id`, `nome_usuario`, `login`, `senha`, `email`) VALUES
 (1, 'Victor ', 'victor', '1234', 'victor@gmail.com'),
 (2, 'Lucas ', 'lucas', '123456', 'lucas@gmail.com'),
 (4, 'Marreta', 'marreta', '123456', 'marreta@gmail.com');
@@ -145,7 +124,7 @@ INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `login`, `senha`, `email`) 
 --
 
 CREATE TABLE `veiculo` (
-  `id_veiculo` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `placa` varchar(45) NOT NULL,
   `marca` varchar(45) NOT NULL,
   `combustivel` varchar(45) NOT NULL,
@@ -157,7 +136,7 @@ CREATE TABLE `veiculo` (
 -- Despejando dados para a tabela `veiculo`
 --
 
-INSERT INTO `veiculo` (`id_veiculo`, `placa`, `marca`, `combustivel`, `cor`, `usuario_id_usuario`) VALUES
+INSERT INTO `veiculo` (`id`, `placa`, `marca`, `combustivel`, `cor`, `usuario_id_usuario`) VALUES
 (3, 'gtx-3060', 'BMW M2', 'gasolina', 'azul', 1),
 (5, 'rtx-9875', 'corsa', 'acool', 'prata', 1);
 
@@ -172,39 +151,31 @@ ALTER TABLE `manutencao`
   ADD PRIMARY KEY (`id_manutencao`),
   ADD KEY `fk_manurtenção_veiculo1` (`veiculo_id_veiculo`),
   ADD KEY `fk_manurtenção_oficina1` (`oficina_id_oficina`),
-  ADD KEY `fk_manurtenção_peças1` (`pecas_id_peca`);
+  ADD KEY `fk_manurtenção_peças1` (`pecas`);
 
 --
 -- Índices de tabela `oficina`
 --
 ALTER TABLE `oficina`
-  ADD PRIMARY KEY (`id_oficina`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `pecas`
 --
 ALTER TABLE `pecas`
-  ADD PRIMARY KEY (`id_peca`);
-
---
--- Índices de tabela `revisao`
---
-ALTER TABLE `revisao`
-  ADD PRIMARY KEY (`id_revisao`),
-  ADD KEY `fk_revisão_veiculo1` (`veiculo_id_veiculo`),
-  ADD KEY `fk_revisão_oficina1` (`oficina_id_oficina`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `veiculo`
 --
 ALTER TABLE `veiculo`
-  ADD PRIMARY KEY (`id_veiculo`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_veiculo_usuario` (`usuario_id_usuario`);
 
 --
@@ -215,37 +186,31 @@ ALTER TABLE `veiculo`
 -- AUTO_INCREMENT de tabela `manutencao`
 --
 ALTER TABLE `manutencao`
-  MODIFY `id_manutencao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_manutencao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de tabela `oficina`
 --
 ALTER TABLE `oficina`
-  MODIFY `id_oficina` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `pecas`
 --
 ALTER TABLE `pecas`
-  MODIFY `id_peca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `revisao`
---
-ALTER TABLE `revisao`
-  MODIFY `id_revisao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `veiculo`
 --
 ALTER TABLE `veiculo`
-  MODIFY `id_veiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para tabelas despejadas
@@ -255,22 +220,15 @@ ALTER TABLE `veiculo`
 -- Restrições para tabelas `manutencao`
 --
 ALTER TABLE `manutencao`
-  ADD CONSTRAINT `fk_manurtenção_oficina1` FOREIGN KEY (`oficina_id_oficina`) REFERENCES `oficina` (`id_oficina`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_manurtenção_peças1` FOREIGN KEY (`pecas_id_peca`) REFERENCES `pecas` (`id_peca`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_manurtenção_veiculo1` FOREIGN KEY (`veiculo_id_veiculo`) REFERENCES `veiculo` (`id_veiculo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Restrições para tabelas `revisao`
---
-ALTER TABLE `revisao`
-  ADD CONSTRAINT `fk_revisão_oficina1` FOREIGN KEY (`oficina_id_oficina`) REFERENCES `oficina` (`id_oficina`),
-  ADD CONSTRAINT `fk_revisão_veiculo1` FOREIGN KEY (`veiculo_id_veiculo`) REFERENCES `veiculo` (`id_veiculo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_manurtenção_oficina1` FOREIGN KEY (`oficina_id_oficina`) REFERENCES `oficina` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_manurtenção_peças1` FOREIGN KEY (`pecas`) REFERENCES `pecas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_manurtenção_veiculo1` FOREIGN KEY (`veiculo_id_veiculo`) REFERENCES `veiculo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Restrições para tabelas `veiculo`
 --
 ALTER TABLE `veiculo`
-  ADD CONSTRAINT `fk_veiculo_usuario` FOREIGN KEY (`usuario_id_usuario`) REFERENCES `usuario` (`id_usuario`);
+  ADD CONSTRAINT `fk_veiculo_usuario` FOREIGN KEY (`usuario_id_usuario`) REFERENCES `usuario` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
